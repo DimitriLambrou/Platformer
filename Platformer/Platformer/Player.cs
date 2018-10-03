629cc00a31d98083cf80b9a508796eee59b962f4
Dimitri Lambrou <dimitrilambrou1@gmail.com>
2018-10-03 16:55:11 +1000

    Collision with slope
    Added code for slope collisions

--- Platformer/Platformer/Player.cs
+++ Platformer/Platformer/Player.cs
@@ -65,6 +65,9 @@ namespace Platformer
 
             playerSprite.velocity = localAcceleration * deltaTime;
             playerSprite.position += playerSprite.velocity * deltaTime;
+
+            collision.game = game;
+            playerSprite = collision.CollideWithPlatforms(playerSprite, deltaTime);
         }
         public void Update(float deltaTime)
         {
